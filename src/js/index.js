3ee550474283d29a0eb436638247f1b745a0370f
pashakiz <pashakiz@gmail.com>
2022-11-03 22:51:43 +0300

    feat: search done

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,49 @@
 import '@scss/main.scss'
-import './renderItems';
+import itemTemplate from './itemTemplate';
+import { getData } from './data';
 
 const dashboard = document.querySelector("#dashboard");
+const searchForm = document.querySelector("#search-form");
+const searchInput = document.querySelector("#search-input");
+const searchMessage = location.href.split('?search=')[1] ? location.href.split('?search=')[1] : '';
 
+//render
+async function renderItems() {
+  searchInput.value = searchMessage ? searchMessage : searchInput.value;
+  const data = await getData();
+  const items = data.filter(row => row.title.toLowerCase().includes(searchInput.value)).map(itemTemplate);
+  dashboard.innerHTML = '';
+  items.forEach(el => {
+    dashboard.innerHTML += el;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', renderItems);
+
+
+//search
+const handleClickSearch = (event) => {
+  if (event.target.closest('.search__icon') === null)
+    return false
+  search(searchInput.value);
+}
+
+const handleKeySearch = (event) => {
+  if (event.code !== 'Enter')
+    return false
+  search(searchInput.value);
+}
+
+const search = (message) => {
+  let url = message ? '/?search=' + message.toLowerCase() : '/';
+  history.pushState(null, null, url);
+  renderItems();
+}
+
+searchForm.addEventListener('click', handleClickSearch);
+searchForm.addEventListener('keyup', handleKeySearch);
+
+//checkbox
 const handleClickItem = (event) => {
   if (event.target.closest('.item') === null)
     return false
